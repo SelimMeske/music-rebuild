fdd4594ae66270105a7bb9f2295acf500d3f9255
SelimMeske <mesic.selim@outlook.com>
2021-05-25 21:32:50 +0200

    Finished the navbar

--- music/src/app/app.module.ts
+++ music/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NavbarComponent } from 'src/components/navbar/navbar.component';
-import { HomeComponent } from 'src/components/home/home.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from './app-material/app-material.module';
+
+/**Components */
+import { NavbarComponent } from 'src/components/navbar/navbar.component';
+import { HomeComponent } from 'src/components/home/home.component';
 
 @NgModule({
   declarations: [
