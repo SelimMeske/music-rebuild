410b6705f89237ca42ef7953782326348265b436
SelimMeske <mesic.selim@outlook.com>
2021-05-26 18:37:37 +0200

    added backend service

--- music/src/app/app.module.ts
+++ music/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from './app-material/app-material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 /**Components */
 import { NavbarComponent } from 'src/components/navbar/navbar.component';
@@ -20,7 +21,8 @@ import { HomeComponent } from 'src/components/home/home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AppMaterialModule
+    AppMaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ music/src/services/backend.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class BackendService {
+    constructor(private http: HttpClient){
+
+    }
+}
